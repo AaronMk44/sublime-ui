b39d1d20e74ce823cef5dcc84cd410baf2c6c09b
Aaron Mkandawire <aaronmk2001@gmail.com>
2026-06-18 10:20:23 +0200

    chore: use ESLint 9 flat config (eslint.config.js)

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  { ignores: ['**/dist/', '**/node_modules/', 'sandbox/', '**/*.cjs'] },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  prettier,
+];
